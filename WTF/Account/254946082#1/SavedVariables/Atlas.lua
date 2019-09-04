bc6af75293d6172982afa12e99ccb90053a0be00
Abhishek Agrwal <abhishekagrwal93@gmail.com>
2019-09-05 01:28:42 +0530

    Added Addons atlas atlas_classicwow spy
    Config changes

---
+++ WTF/Account/254946082#1/SavedVariables/Atlas.lua
@@ -0,0 +1,27 @@
+
+AtlasDB = {
+	["profileKeys"] = {
+		["Krombopulos - Noggenfogger"] = "Default",
+	},
+	["profiles"] = {
+		["Default"] = {
+			["options"] = {
+				["autoSelect"] = true,
+				["frames"] = {
+					["lock"] = true,
+				},
+				["dropdowns"] = {
+					["module"] = 2,
+					["menuType"] = 5,
+				},
+			},
+			["dropdowns"] = {
+				["Eastern Kingdoms Instances"] = 8,
+			},
+			["minimap"] = {
+				["minimapPos"] = 324.057888128618,
+				["hide"] = true,
+			},
+		},
+	},
+}
---
+++ WTF/Account/254946082#1/SavedVariables/Spy.lua
@@ -0,0 +1,67 @@
+
+SpyDB = {
+	["kosData"] = {
+		["Noggenfogger"] = {
+			["Horde"] = {
+				["Krombopulos"] = {
+					["Borex"] = {
+						["mapID"] = 1413,
+						["guild"] = "Wet Tabard Contest",
+						["zone"] = "The Barrens",
+						["mapY"] = 0.37,
+						["class"] = "HUNTER",
+						["added"] = 1567625418,
+						["isEnemy"] = true,
+						["isGuess"] = false,
+						["mapX"] = 0.62,
+						["time"] = 1567625418,
+						["name"] = "Borex",
+						["subZone"] = "Ratchet",
+						["level"] = 27,
+						["race"] = "Dwarf",
+						["kos"] = 1,
+					},
+				},
+			},
+		},
+	},
+	["removeKOSData"] = {
+		["Noggenfogger"] = {
+			["Horde"] = {
+			},
+		},
+	},
+	["profileKeys"] = {
+		["Krombopulos - Noggenfogger"] = "Krombopulos - Noggenfogger",
+	},
+	["profiles"] = {
+		["Krombopulos - Noggenfogger"] = {
+			["MainWindow"] = {
+				["Position"] = {
+					["y"] = 489.500061035156,
+					["x"] = 13,
+					["w"] = 186.999969482422,
+					["h"] = 34.9999771118164,
+				},
+			},
+			["OnlySoundKoS"] = true,
+			["WarnOnKOSGuild"] = false,
+			["Colors"] = {
+				["Bar"] = {
+					["Bar Text"] = {
+						["a"] = 1,
+					},
+				},
+			},
+			["HideSpy"] = true,
+			["AppendUnitNameCheck"] = true,
+			["ShowKoSButton"] = true,
+			["ShowNearbyList"] = false,
+			["Locked"] = true,
+			["MainWindowVis"] = false,
+			["ShowOnDetection"] = false,
+			["AppendUnitKoSCheck"] = true,
+			["OnlyAnnounceKoS"] = true,
+		},
+	},
+}
